8b477686abab11c1f5913e25b3ddc9277a4e1904
Marko Schuetz-Schmuck <MarkoSchuetz@web.de>
2024-03-20 18:31:25 -0400

    totals for lecture topic tasks get in the way: removed

--- exportMetricsForLectureTopicTasks.py
+++ exportMetricsForLectureTopicTasks.py
@@ -12,7 +12,7 @@ def write_lecture_topic_task_data_to_csv(
     with open(csv_file_path, mode="w", newline="") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["Developer", "Lecture Topic Tasks Completed", "Met Quota"])
-        writer.writerow(["Total", ltt_data.totalLectureTopicTasks, "N/A"])
+        # writer.writerow(["Total", ltt_data.totalLectureTopicTasks, "N/A"])
         for developer, tasks_closed in ltt_data.lectureTopicTasksByDeveloper.items():
             writer.writerow([developer, tasks_closed, tasks_closed >= task_quota])
 
