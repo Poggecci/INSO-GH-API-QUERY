af298f8a62fa938f79e16a2711819e1fafb619f5
Kelvin Gonzalez <kelvin.gonzalez11@upr.edu>
2025-03-20 23:03:09 -0400

    Make it so all uses of lecture topic task data read from the same implementation

--- src/generateLectureTopicTaskMetrics.py
+++ src/generateLectureTopicTaskMetrics.py
@@ -1,82 +1,29 @@
+import logging
+from src.generateTeamMetrics import (
+    fetchProcessedIssues,
+    getLectureTopicTaskMetricsFromIssues,
+)
 from src.utils.models import LectureTopicTaskData
-
-from src.utils.queryRunner import runGraphqlQuery
-
-get_team_lecture_topic_tasks = """
-query QueryProjectItemsForTeam($owner: String!, $team: String!,
-  $nextPage: String)
-{
-  organization(login: $owner) {
-    projectsV2(query: $team, first: 100) {
-      nodes{
-        title
-        items(first: 100, after: $nextPage) {
-          pageInfo {
-            endCursor
-            hasNextPage
-          }
-          nodes {
-            content {
-              ... on Issue {
-                title
-                createdAt
-                closed
-                assignees(first:20) {
-                  nodes{
-                    login
-                  }
-                }
-              }
-            }
-          }   
-        }
-      }
-    }
-  }
-}
-
-"""
 
 
 def getLectureTopicTaskMetrics(
-    org: str, team: str, members: list[str]
+    org: str,
+    team: str,
+    members: list[str],
+    managers: list[str],
+    logger: logging.Logger | None = None,
+    shouldCountOpenIssues: bool = False,
 ) -> LectureTopicTaskData:
-    lectureTopicTaskData = LectureTopicTaskData()
-    lectureTopicTaskData.lectureTopicTasksByDeveloper = {
-        member: 0 for member in members
-    }
-
-    params = {"owner": org, "team": team}
-    hasAnotherPage = True
-    while hasAnotherPage:
-        response = runGraphqlQuery(query=get_team_lecture_topic_tasks, variables=params)
-        projects: list[dict] = response["organization"]["projectsV2"]["nodes"]
-        project = next(filter(lambda x: x["title"] == team, projects), None)
-        if not project:
-            raise Exception(
-                "Project not found in org. Likely means the project"
-                " board doesn't share the same name as the team."
-            )
-        # Extract data
-        issues = project["items"]["nodes"]
-        for issue in issues:
-            # don't count open issues
-            if not issue["content"].get("closed", False):
-                continue
-            # Expect LTTs to be properly titled as such
-            if "[Lecture Topic Task]" not in issue["content"].get("title", ""):
-                continue
-            lectureTopicTaskData.totalLectureTopicTasks += 1
-            for dev in issue["content"]["assignees"]["nodes"]:
-                if dev["login"] not in members:
-                    raise Exception(
-                        f"Task assigned to developer {dev['login']} not"
-                        " belonging to the team"
-                    )
-                lectureTopicTaskData.lectureTopicTasksByDeveloper[dev["login"]] += 1
-
-        hasAnotherPage = project["items"]["pageInfo"]["hasNextPage"]
-        if hasAnotherPage:
-            params["nextPage"] = project["items"]["pageInfo"]["endCursor"]
-
-    return lectureTopicTaskData
+    if logger is None:
+        logger = logging.getLogger(__name__)
+    return getLectureTopicTaskMetricsFromIssues(
+        issues=fetchProcessedIssues(
+            org=org,
+            team=team,
+            logger=logger,
+            managers=managers,
+            shouldCountOpenIssues=shouldCountOpenIssues,
+        ),
+        members=members,
+        logger=logger,
+    )
--- src/generateTeamMetrics.py
+++ src/generateTeamMetrics.py
@@ -1,6 +1,8 @@
 from collections.abc import Iterator, ValuesView
 import logging
 from datetime import datetime
+from queue import Queue
+from threading import Thread
 from src.getMilestones import getMilestones
 from src.getProject import getProject
 from src.utils.constants import pr_tz
@@ -10,8 +12,15 @@ from src.utils.issues import (
     parseIssue,
     shouldCountIssue,
 )
-from src.utils.models import DeveloperMetrics, MilestoneData, ParsingError
+from src.utils.models import (
+    DeveloperMetrics,
+    Issue,
+    LectureTopicTaskData,
+    MilestoneData,
+    ParsingError,
+)
 from src.utils.queryRunner import runGraphqlQuery
+from concurrent.futures import ThreadPoolExecutor
 
 # Check out https://docs.github.com/en/graphql/guides/introduction-to-graphql#schema to understand this query better
 get_team_issues = """
@@ -185,6 +194,93 @@ def fetchIssuesFromGithub(
             ]
 
 
+def fetchProcessedIssues(
+    *,
+    org: str,
+    team: str,
+    logger: logging.Logger,
+    hooks: list[str] | None = None,
+    milestone: str | None = None,
+    startDate: datetime | None = None,
+    endDate: datetime | None = None,
+    managers: list[str],
+    shouldCountOpenIssues: bool = False,
+) -> Iterator[Issue]:
+    for issue_dict in fetchIssuesFromGithub(org=org, team=team, logger=logger):
+        try:
+            issue = parseIssue(issue_dict=issue_dict)
+        except ParsingError:
+            # don't log since the root cause can be hard to identify without manual review
+            continue
+        except KeyError as e:
+            logger.exception(
+                f"{e}. GH GraphQL API Issue type may have changed. This requires updating the code. Please contact the maintainers."
+            )
+            continue
+        except ValueError as e:
+            logger.exception(
+                f"{e}. GH GraphQL API Issue type may have changed. This requires updating the code. Please contact the maintainers."
+            )
+            continue
+
+        # Apply any overrides prior to counting or discarding the issue
+        if (
+            hooks is not None
+            and milestone is not None
+            and startDate is not None
+            and endDate is not None
+        ):
+            issue = applyIssuePreProcessingHooks(
+                hooks=hooks,
+                issue=issue,
+                milestone=milestone,
+                startDate=startDate,
+                endDate=endDate,
+            )
+
+        if not shouldCountIssue(
+            issue=issue,
+            logger=logger,
+            currentMilestone=milestone,
+            managers=managers,
+            shouldCountOpenIssues=shouldCountOpenIssues,
+        ):
+            continue
+
+        print(f"Successfully validated Issue #{issue.number}")
+        yield issue
+
+
+def getLectureTopicTaskMetricsFromIssues(
+    issues: Iterator[Issue], members: list[str], logger: logging.Logger
+) -> LectureTopicTaskData:
+    lectureTopicTaskData = LectureTopicTaskData()
+    lectureTopicTaskData.lectureTopicTasksByDeveloper = {
+        member: 0 for member in members
+    }
+
+    for issue in issues:
+        if not issue.isLectureTopicTask:
+            continue
+        if len(issue.assignees) != 1:
+            logger.warning(
+                f"[Issue #{issue.number}]({issue.url}) does not have only 1 assigned developer so lecture topic task points for this issue will be ignored"
+            )
+            continue
+        lectureTopicTaskData.lectureTopicTasksByDeveloper[issue.assignees[0]] += 1
+        lectureTopicTaskData.totalLectureTopicTasks += 1
+
+    return lectureTopicTaskData
+
+
+def iteratorSplitter(iterator: Iterator, queue1: Queue, queue2: Queue):
+    for item in iterator:
+        queue1.put(item)
+        queue2.put(item)
+    queue1.put(None)
+    queue2.put(None)
+
+
 def getTeamMetricsForMilestone(
     *,
     org: str,
@@ -239,7 +335,6 @@ def getTeamMetricsForMilestone(
     developers = [member for member in members if member not in managers]
     devPointsClosed = {dev: 0.0 for dev in developers}
     devTasksCompleted = {dev: [0 for _ in range(sprints)] for dev in developers}
-    devLectureTopicTasks = {dev: 0 for dev in developers}
     totalPointsClosed = 0.0
     milestoneData = MilestoneData(sprints=sprints, startDate=startDate, endDate=endDate)
     sprintCutoffs = generateSprintCutoffs(
@@ -248,81 +343,73 @@ def getTeamMetricsForMilestone(
     devPointsByLabel = {dev: {} for dev in developers}
     milestoneLabels = set()
 
-    # Fetch all issues for the team, drop invalid ones, and apply their points to the correct developers
-    for issue_dict in fetchIssuesFromGithub(org=org, team=team, logger=logger):
-        try:
-            issue = parseIssue(issue_dict=issue_dict)
-        except ParsingError:
-            # don't log since the root cause can be hard to identify without manual review
-            continue
-        except KeyError as e:
-            logger.exception(
-                f"{e}. GH GraphQL API Issue type may have changed. This requires updating the code. Please contact the maintainers."
-            )
-            continue
-        except ValueError as e:
-            logger.exception(
-                f"{e}. GH GraphQL API Issue type may have changed. This requires updating the code. Please contact the maintainers."
-            )
-            continue
+    issues = fetchProcessedIssues(
+        org=org,
+        team=team,
+        logger=logger,
+        hooks=issuePreProcessingHooks,
+        milestone=milestone,
+        startDate=startDate,
+        endDate=endDate,
+        managers=managers,
+        shouldCountOpenIssues=shouldCountOpenIssues,
+    )
 
-        # Apply any overrides prior to counting or discarding the issue
-        issue = applyIssuePreProcessingHooks(
-            hooks=issuePreProcessingHooks,
-            issue=issue,
-            milestone=milestone,
-            startDate=startDate,
-            endDate=endDate,
+    # Split issues iterator to read for both issue metrics and lecture topic task metrics
+    issueMetricsQueue, lectureTopicTaskQueue = Queue(), Queue()
+    thread = Thread(
+        target=iteratorSplitter, args=(issues, issueMetricsQueue, lectureTopicTaskQueue)
+    )
+    thread.start()
+
+    with ThreadPoolExecutor() as executor:
+        # Run concurrently to obtain lecture topic task data in parallel with issue metrics data
+        future = executor.submit(
+            getLectureTopicTaskMetricsFromIssues,
+            iter(lectureTopicTaskQueue.get, None),
+            members,
+            logger,
         )
 
-        if not shouldCountIssue(
-            issue=issue,
-            logger=logger,
-            currentMilestone=milestone,
-            managers=managers,
-            shouldCountOpenIssues=shouldCountOpenIssues,
-        ):
-            continue
-
-        print(f"Successfully validated Issue #{issue.number}")
-        issueMetrics = calculateIssueScores(
-            issue=issue,
-            managers=managers,
-            developers=developers,
-            startDate=startDate,
-            endDate=endDate,
-            useDecay=useDecay,
-            logger=logger,
-        )
-        # attribute base issue points to developer alongside giving them credit for the completed task
-        for dev, score in issueMetrics.pointsByDeveloper.items():
-            devPointsClosed[dev] += score
-            # attribute task completion to appropriate sprint
-            taskCompletionDate = (
-                issue.closedAt if issue.closedAt is not None else issue.createdAt
+        # Fetch all issues for the team, drop invalid ones, and apply their points to the correct developers
+        for issue in iter(issueMetricsQueue.get, None):
+            issueMetrics = calculateIssueScores(
+                issue=issue,
+                managers=managers,
+                developers=developers,
+                startDate=startDate,
+                endDate=endDate,
+                useDecay=useDecay,
+                logger=logger,
             )
-            sprintIndex = getCurrentSprintIndex(
-                date=taskCompletionDate, cutoffs=sprintCutoffs
-            )
-            devTasksCompleted[dev][sprintIndex] += 1
-            # update total points closed metric
-            totalPointsClosed += score
-
-            # attribute Lecture topic tasks if applicable
-            if issue.isLectureTopicTask:
-                devLectureTopicTasks[dev] += 1
-
-            # assign issue score to labels per developer
-            for label in issue.labels:
-                devPointsByLabel[dev][label] = (
-                    devPointsByLabel[dev].get(label, 0) + score
+            # attribute base issue points to developer alongside giving them credit for the completed task
+            for dev, score in issueMetrics.pointsByDeveloper.items():
+                devPointsClosed[dev] += score
+                # attribute task completion to appropriate sprint
+                taskCompletionDate = (
+                    issue.closedAt if issue.closedAt is not None else issue.createdAt
                 )
-                milestoneLabels.add(label)
+                sprintIndex = getCurrentSprintIndex(
+                    date=taskCompletionDate, cutoffs=sprintCutoffs
+                )
+                devTasksCompleted[dev][sprintIndex] += 1
+                # update total points closed metric
+                totalPointsClosed += score
 
-        # attribute bonuses for developers
-        for dev, bonus in issueMetrics.bonusesByDeveloper.items():
-            devPointsClosed[dev] += bonus
-            # Note that bonus do not increase the total points closed such as to not "raise the bar"
+                # assign issue score to labels per developer
+                for label in issue.labels:
+                    devPointsByLabel[dev][label] = (
+                        devPointsByLabel[dev].get(label, 0) + score
+                    )
+                    milestoneLabels.add(label)
+
+            # attribute bonuses for developers
+            for dev, bonus in issueMetrics.bonusesByDeveloper.items():
+                devPointsClosed[dev] += bonus
+                # Note that bonus do not increase the total points closed such as to not "raise the bar"
+
+        # Obtain lecture topic task metrics result
+        lectureTopicTaskData = future.result()
 
     untrimmedAverage = totalPointsClosed / max(1, len(devPointsClosed))
     trimmedAverage = outliersRemovedAverage(devPointsClosed.values())
@@ -359,9 +446,13 @@ def getTeamMetricsForMilestone(
             pointsClosed=devPointsClosed[dev],
             percentContribution=contribution * 100.0,
             expectedGrade=expectedGrade,
-            lectureTopicTasksClosed=devLectureTopicTasks[dev],
+            lectureTopicTasksClosed=lectureTopicTaskData.lectureTopicTasksByDeveloper[
+                dev
+            ],
             pointPercentByLabel={
-                label: devPointsByLabel[dev].get(label, 0) / max(1, devPointsClosed[dev]) * 100
+                label: devPointsByLabel[dev].get(label, 0)
+                / max(1, devPointsClosed[dev])
+                * 100
                 for label in milestoneLabels
             },
         )
