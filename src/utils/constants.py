a9a1ccb73e1335704b2674011b915dc691b4777a
KelvinGonzalez <69769392+KelvinGonzalez@users.noreply.github.com>
2025-05-05 17:08:38 -0400

    Revert "Dump generated metrics as json files for history diff"

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -6,8 +6,6 @@ from datetime import time
 pr_tz = pytz.timezone("America/Puerto_Rico")
 default_start_time = time(hour=8, minute=0)
 default_end_time = time(hour=20, minute=0)
-json_time_placeholder = "[time]"
-json_dumps_dir = "dumps"
 
 
 def getToken():
